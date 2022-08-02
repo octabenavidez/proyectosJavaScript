fe658c01200eabf4f9b21de0a9b2cada9b9d9303
octabenavidez <octabenavidezsarmiento@gmail.com>
2022-08-01 23:56:51 -0300

    Tienda virtual v2

--- js.js
+++ js.js
@@ -1,58 +1,84 @@
-alert("Bienvenid@ a la tienda virtual de Bad Bunny")
+class Prenda {
+    constructor (id, producto, precio){
+        this.id = id
+        this.producto = producto
+        this.precio = precio
+    }
 
-alert("Contamos con el stock de Remera, Pantalon, Zapatillas")
-
-let prenda = prompt("Ingrese la prenda que desea")
-let precio
-let codigo
-let impuestos
-let entrada
-
-function descuento(objeto){
-    return objeto * 0.25
-}
-
-function calcularIva(objeto){
-    return objeto * 1.21
-}
-
-switch (prenda.toLowerCase()){
-    case "remera":
-        precio = 200
-        break
-    case "pantalon":
-        precio = 150
-        break
-    case "zapatillas":
-        precio = 250
-        break 
-    default:
-        alert("No tenemos stock de esa prenda")
-        break
-}
-
-alert(prenda.toLowerCase() + " tiene un costo de $" + precio )
-
-impuestos = prompt("¿Quiere ver el precio final de su producto? (impuestos agregados)")
-
-if (impuestos.toLowerCase() === "si") {
-    entrada = prompt("¿Tiene un codigo de descuento?")
-    if (entrada.toLowerCase() === "si"){
-        for (let i = 0; i < 3; i++){
-            codigo = prompt("Ingrese su codigo de descuento:")
-            if (codigo === "Coder"){
-                alert("Codigo aplicado! el costo final de su prenda es de: $" + (calcularIva(precio) - descuento(precio))) 
-                break
-            } else if (i === 2){
-                alert("Supero la cantidad de intentos de codigo de descuento, El costo final de su prenda sin descuento es de: $" + calcularIva(precio))
-            }
-        }
-    } else{
-        alert("El costo final de su prenda es de: $" + calcularIva(precio))
+    calcularIva(){
+        this.precio = this.precio * 1.21
     }
 }
+
+const mercaderia = [
+    new Prenda(1, "remera", 200),
+    new Prenda(2, "pantalon", 150),
+    new Prenda(3, "zapatillas", 250),
+    new Prenda(4, "medias", 50),
+    new Prenda(5, "lentes", 75)
+]
+
+const listaNombres = (lista) => {
+    let prendas = []
+    for (const el of lista){
+        prendas.push(el.producto)
+    }
+
+    return prendas
+}
+
+const borrarCarrito = (elemento) => {
+    let indice = listaNombres(carrito).indexOf(elemento)
+
+    if (indice != -1 ) {
+        carrito.splice(indice, 1)
+    } 
+}
+
+const agregarCarrito = (prenda) => {
+    let indice = listaNombres(mercaderia).indexOf(prenda.toLowerCase())
+
+    if (indice != -1){
+        carrito.push(mercaderia[indice]) 
+    }
+}
+
+alert("Bienvenid@ a la tienda virtual de Bad Bunny")
+
+alert("Contamos con el stock de: " + listaNombres(mercaderia).join(", "))
+
+let carrito = []
+let prenda = agregarCarrito(prompt("Ingrese la prenda que desea: "))
+let entrada
+
+entrada = prompt("¿Quiere comprar otra prenda?")
+
+while (entrada.toLowerCase() != "no"){
+    prenda = agregarCarrito(prompt("Ingrese la prenda que desea: "))
+    entrada = prompt("¿Quiere comprar otra prenda?")
+}
+
+entrada = prompt("¿Quiere eliminar de su carrito alguna prenda?")
+
+if (entrada.toLowerCase() === "si"){
+    alert("Usted tiene estas prendas: " + listaNombres(carrito).join(", "))
+    prenda = prompt("Ingrese el nombre de la prenda a eliminar: ")
+    borrarCarrito(prenda.toLowerCase())
+} 
+
+entrada = prompt("¿Desea ver el precio total de sus productos? (Incluye IVA)")
+
+if (entrada.toLowerCase() === "si"){
+    let total = 0
+    for (let i = 0; i < carrito.length; i++){
+        carrito[i].calcularIva()
+        total = total + carrito[i].precio
+    }
+    alert("El total de las prendas que ha seleccionado es: $" + total)
+} 
 
 alert("Regrese cuando quiera!")
 
 console.log("Fin del programa")
+
 
