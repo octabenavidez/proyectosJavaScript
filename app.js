9e60516fb887d56f6bd369723ed4addb5f1fb1d2
octabenavidez <octabenavidezsarmiento@gmail.com>
2022-09-01 23:44:30 -0300

    Añadiendo fetch

--- app.js
+++ app.js
@@ -13,12 +13,49 @@ class Prenda {
     }
 }
 
-const mercaderia = [
-    new Prenda(1, "remera", 200, "A2F3"),
-    new Prenda(3, "buzo", 250, "A89Z"),
-    new Prenda(4, "gorro", 50, "BO90"),
-    new Prenda(5, "gorra", 75, "X214")
-]
+// Api con fetch
+
+const monedaEl_one = document.getElementById('moneda-uno');
+const monedaEl_two = document.getElementById('moneda-dos');
+const cantidadEl_one = document.getElementById('cantidad-uno');
+const cantidadEl_two = document.getElementById('cantidad-dos');
+const cambioEl = document.getElementById('cambio');
+const tazaEl = document.getElementById('taza');
+
+
+// Fetch Exchange Rate api
+function calculate(){
+    const moneda_one = monedaEl_one.value;
+    const moneda_two = monedaEl_two.value;
+
+   fetch(`https://api.exchangerate-api.com/v4/latest/${moneda_one}`)
+   .then(res => res.json() )
+   .then(data => {
+       const taza = data.rates[moneda_two];
+       
+       cambioEl.innerText = `1 ${moneda_one} = ${taza} ${moneda_two}`;
+
+       cantidadEl_two.value = (cantidadEl_one.value * taza).toFixed(2);
+
+    } );
+    
+}
+
+//Event listeners
+monedaEl_one.addEventListener('change', calculate);
+cantidadEl_one.addEventListener('input', calculate);
+monedaEl_two.addEventListener('change', calculate);
+cantidadEl_two.addEventListener('input', calculate);
+
+taza.addEventListener('click', () =>{
+    const temp = monedaEl_one.value;
+    monedaEl_one.value = monedaEl_two.value;
+    monedaEl_two.value = temp;
+    calculate();
+} );
+
+
+calculate();
 
 
 // Reconocimiento de usuario
@@ -97,7 +134,7 @@ function crearEnTabla(pr) {
 } 
 
 btnGorro.addEventListener("click", () => {
-    const nuevaPrenda = new Prenda(4, "gorro", 50, "I2P1")
+    const nuevaPrenda = new Prenda (4, "gorro", 50, "BO90")
     nuevaPrenda.calcularIva()
     carrito.push(nuevaPrenda)
 
